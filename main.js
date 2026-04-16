78b5292d74ccfddc4dc9e172dfb8586fce723650
ashishkumarworke2-stack <ashishkumarworke2@gmail.com>
2026-04-16 11:16:32 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 const RPC_URLS = [
-  "https://mainnet.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c",
-  "https://beta.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c"
+  https://<solana>.quicknode.com/<key>/<QN_f89359e450ab41b5af9b8ede476755a6>"
 ];
