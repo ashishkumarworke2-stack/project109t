621db759e8c60b8e53186a9fd2d5f8ae9130aeec
ashishkumarworke2-stack <ashishkumarworke2@gmail.com>
2026-04-16 10:16:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-const RPC_URL = 'wss://mainnet.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c';
+const RPC_URLS = [
+  "https://mainnet.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c",
+  "https://beta.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c"
+];
