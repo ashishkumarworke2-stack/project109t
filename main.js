d621b8fb8129160848e70701dc2b4a28e730e629
ashishkumarworke2-stack <ashishkumarworke2@gmail.com>
2026-04-18 01:20:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-const RPC_URLS = [
-  https://<solana>.quicknode.com/<key>/<QN_f89359e450ab41b5af9b8ede476755a6>"
-];
+// server.js
+const express = require('express');
+const app = express();
+
+app.get('/api/rpc', async (req, res) => {
+  const response = await fetch('https://mainnet.helius-rpc.com/?api-key=70da5c4d-b87b-4842-bfd5-a9977e4aed1c', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(req.body)
+  });
+  const data = await response.json();
+  res.json(data);
+});
+
+app.listen(3001);
